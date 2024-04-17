c9ec4761b67856742c376aab479e55d8652f3a84
Zachary Vivian <161660633+zacharycvivian@users.noreply.github.com>
2024-04-17 10:54:17 -0500

    Added create account button to login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 import { VmViewComponent } from './vm-view/vm-view.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'student-dashboard', component: StudentDashboardComponent },
   { path: 'teacher-dashboard', component: TeacherDashboardComponent },
   { path: 'vm-view', component: VmViewComponent },
+  { path: 'create-account', component: CreateAccountComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,11 +9,9 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   constructor(private router: Router) {}
 
-  //Dummy data for redirecting to teacher or student dashboards, will need to use the SQL database and validate authorization information in the dashboards so users cannot change the url to /student-dashboard or /teacher-dashboard and edit data
   login(username: string, password: string) {
-    console.log(username, password); 
+    console.log(username, password);
     if (username === 'teacher' && password === 'password') {
-      console.log('Redirecting to teacher dashboard');
       this.router.navigate(['/teacher-dashboard']);
     } else if (username === 'student' && password === 'password') {
       this.router.navigate(['/student-dashboard']);
@@ -22,5 +20,8 @@ export class LoginComponent {
       alert('Invalid credentials');
     }
   }
-  
+
+  redirectToCreateAccount() {
+    this.router.navigate(['/create-account']);
+  }
 }
